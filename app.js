d81035cfd8c820156674dfba966e022dc1f4a600
Jonathan Maddison <jonathanwmaddison@gmail.com>
2017-05-15 11:28:42 -0700

    added tweet model, index route, and app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var  express = require('express');
+var mongoose = require('mongoose');
+var Tweets = require('./models/tweets');
+var indexRoute = require('./routes/index');
+
+var app = express()
+
+//Connect to Database
+var database = 'mongodb://localhost/motivate_my_run';
+mongoose.connect(database);
+
+//Set up routes
+app.use("/",indexRoute)
+
+app.listen("3000", function() {
+    console.log("Twitter Scraper has started on port 3000")
+})
---
+++ models/tweets.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var tweetSchema = new mongoose.Schema({
+    username: String,
+    text: String,
+    images: Array,
+    url: String,
+    date: Date,
+    tweetId: String,
+    sent: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Tweets', tweetSchema)
