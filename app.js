93eab5dba8a330aa2efc815fcff792ef0c30e447
Jonathan Maddison <jonathanwmaddison@gmail.com>
2017-05-16 16:33:18 -0700

    set up app.js to run on heroku and added twilio test

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var  express = require('express');
+var express = require('express');
 var mongoose = require('mongoose');
 var Tweets = require('./models/tweets');
 var indexRoute = require('./routes/index');
-var seedDB = require('./seeds');
 
 var app = express()
-
+var port = process.env.port ? process.env.port : "3000"
+var ip = process.env.ip ? process.env.ip : "localhost";
 
 //Connect to Database
 var database = 'mongodb://localhost/motivate_my_run';
 mongoose.connect(database);
 
-seedDB();
+app.use(twilioNotifications.sendMessage);
 
 //Set up routes
 app.use("/",indexRoute)
 
-app.listen("3000", function() {
-    console.log("Twitter Scraper has started on port 3000")
+app.listen(port, ip, function() {
+    console.log("Twitter Scraper has started on port"+port)
 })
