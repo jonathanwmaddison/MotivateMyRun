e895f3ff6fba6381f80110f1bac4e5fa9240f469
Jonathan Maddison <jonathanwmaddison@gmail.com>
2017-05-16 18:01:44 -0700

    fix port and ip settings

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = express()
 var twilioClient = require('./twilioClient');
 var fetchTweet = require('./fetchTweet');
 
-var port = process.env.port ? process.env.port : "3000"
-var ip = process.env.ip ? process.env.ip : "localhost";
+var port = process.env.PORT ? process.env.PORT : "3000"
+var ip = process.env.IP ? process.env.IP : "localhost";
 
 
 //Set up routes
