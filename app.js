645184e7bb376c3b3a98590cc02362dd4803d849
Jonathan Maddison <jonathanwmaddison@gmail.com>
2017-05-16 18:07:14 -0700

    fix port and ip settings

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var twilioClient = require('./twilioClient');
 var fetchTweet = require('./fetchTweet');
 
 var port = process.env.PORT ? process.env.PORT : "3000"
-var ip = process.env.IP ? process.env.IP : "localhost";
 
 
 //Set up routes
@@ -12,6 +11,6 @@ app.get("/", function() {
     fetchTweet(twilioClient.sendSms)    
 })
 
-app.listen(port, ip, function() {
+app.listen(port, function() {
     console.log("Twilio client has started on port "+port)
 })
